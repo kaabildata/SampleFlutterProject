18e6736455800b3b5d355f5ecf24702502bcdb72
abhilash09bme095 <abhilashkollikonda@gmail.com>
2021-05-25 01:13:04 +0530

    Pulling Streams and displaying as list view in Home screen

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -50,19 +50,52 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[100],
+          title: Text(
+            "Users",
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          centerTitle: false,
+        ),
         backgroundColor: Colors.indigo[100],
         body: ConstrainedBox(
           constraints: BoxConstraints(),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  onPressed: () {
-                    messagesStream();
-                  },
-                  child: Text("Print users to console")),
+              StreamBuilder<QuerySnapshot>(
+                builder: (context, snapshot) {
+                  List<Widget> userWidgets = [];
+                  if (snapshot.hasData) {
+                    final users = snapshot.data.docs;
+
+                    for (var user in users) {
+                      Map<String, dynamic> userMap = user.data();
+                      final userName = userMap["Name"];
+                      final userEmail = userMap["Email"];
+                      final userWidget = userBox(
+                        email: userEmail,
+                        name: userName,
+                      );
+                      userWidgets.add(userWidget);
+                    }
+                    return Expanded(
+                      child: ListView(
+                        children: userWidgets,
+                      ),
+                    );
+                  } else {
+                    userWidgets.add(Text("No Data"));
+                  }
+                },
+                stream: _firestore.collection('users').snapshots(),
+              ),
               IconButton(
-                icon: Icon(Icons.close),
+                icon: Icon(
+                  Icons.logout,
+                  size: 25,
+                ),
                 onPressed: () {
                   Navigator.pushNamed(
                     context,
@@ -73,6 +106,54 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class userBox extends StatelessWidget {
+  final String name;
+  final String email;
+
+  const userBox({this.name, this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: CircleAvatar(
+                  backgroundColor: Colors.indigo[100],
+                  child: Icon(
+                    Icons.person,
+                    size: 25,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "$name",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    "$email",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Divider(
+            color: Colors.indigo[50],
+          )
+        ],
       ),
     );
   }
